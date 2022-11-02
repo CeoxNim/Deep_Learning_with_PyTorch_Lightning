651b3a96cb56532b54d91d6cbcf7e3fb9afda19f
CeoxNim <cxn@pku.edu.cn>
2022-11-02 06:10:46 +0000

    Chapter4&5

---
+++ Chapter4/readme.md
@@ -0,0 +1,4 @@
+This Chapter tells us how to use pytorch_lightning flash.  
+However, I think it may be not very useful for me now.  
+So I think skip it.  
+Maybe oneday I'll go back to finish it.  
--- cs.py
+++ cs.py
@@ -1,5 +1,9 @@
-import numpy as np 
-from tqdm import tqdm 
+n = int(input())
 
-for i in tqdm(range(1000000000)):
-    j = i ** 2 + i + 1
+movie_info = {}
+for i in range(n):
+    name, score, num = input().split()
+    movie_info[name] = (int(score), int(num))
+sorted_movie_info  = sorted(movie_info.items(), key=lambda x: (x[1][0], x[1][1]), reverse=True)
+for movie_info in sorted_movie_info:
+    print(movie_info[0])    
